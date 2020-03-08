dc78626da1959754354d0ba45bcbbac03e5f3e0e
Chantal S <chantal.shaib@gmail.com>
2020-03-08 16:50:04 -0400

    2/100, easy, inplace

--- vowel_reverse.py
+++ vowel_reverse.py
@@ -1,23 +1,23 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        recomb_str = []
 
         ptr1 = 0
-        ptr2 = 0
-        s_rev = s[::-1]
+        ptr2 = len(s)-1
         
-        while ptr1 < len(s):
+        s = [char for char in s]
+        
+        while ptr1 < ptr2:
             if s[ptr1] not in vowels:
-                recomb_str.append(s[ptr1])
                 ptr1 += 1
             else:
-                if s_rev[ptr2] not in vowels:
-                    ptr2 += 1
+                if s[ptr2] not in vowels:
+                    ptr2 -= 1
                 else:
-                    recomb_str.append(s_rev[ptr2])
-                    ptr2 += 1
+                    tmp = s[ptr1]
+                    s[ptr1] = s[ptr2]
+                    s[ptr2] = tmp
+                    ptr2 -= 1
                     ptr1 += 1
 
-        return ''.join(recomb_str)
-            
+        return ''.join(s)
